abaf8dc881202b6fac01566856bcef445a812530
Semjon <semjonglushkov@yandex.ru>
2018-05-12 17:22:41 +0300

    liear speed up

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,7 @@ int main(int argc, char *argv[]) { std::cout << get_nprocs() << std::endl;
 	int tn = n / nthreads;
 	double step = (right - left) / nthreads;
 	double integral = 0;
-	std::vector<std::thread> thread_list;
+	std::vector<std::thread*> thread_list;
 	std::vector<double> pres(nthreads);
 	std::cout << "Create threads" << std::endl;
 	/*
@@ -34,12 +34,12 @@ int main(int argc, char *argv[]) { std::cout << get_nprocs() << std::endl;
 	for (int i = 0; i < nthreads; i++) {
 		double ileft = left + i * step;
 		double iright = left + (i + 1) * step;	
-		thread_list.push_back(std::thread(integrate,
-		 				[] (double x) {return std::sin(x);},
+		thread_list.push_back(new std::thread(integrate,
+		 				func,
 						ileft, iright, tn, &(pres[i])));
 	}
 	for (int i = 0; i < nthreads; i++) {
-		thread_list[i].join();
+		(*thread_list[i]).join();
 		integral += pres[i];
 	}
 	std::cout << integral << std::endl;
@@ -55,8 +55,10 @@ double sinx(double x) {
 
 void integrate(double (*func)(double), double left, double right, int n, double *result) {
 	*result = 0;
+	double local_res = 0;
 	double step = (right - left) / n;
 	for(double x = left; x < right; x += step) {
-		*result += func(x) * step;
+		local_res += func(x) * step;
 	}
+	*result = local_res;
 }
